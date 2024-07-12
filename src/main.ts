2bfd78ea12641c0a2c2eb43fb6f6c384abc035cb
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-07-12 20:42:42 +0800

    add angular material

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter([]),
+    provideRouter([]), provideAnimationsAsync(),
   ],
 });
